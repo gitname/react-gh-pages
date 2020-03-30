67c2e25cc4ab7a5fa4518aeed22f0f2e0bb5e602
Rahul Nag <rahulnag625@gmail.com>
2020-03-30 12:22:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload....rahul nag.
         </p>
       </div>
     );
