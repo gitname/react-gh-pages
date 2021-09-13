715523a99106e1485b2d0d34b450f2c0d0c72c81
It's Sappy <57357216+mastrmind-dev@users.noreply.github.com>
2021-09-13 13:47:55 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <img src="https://github.githubassets.com/images/modules/site/social-cards/github-social.png">This is an image</img>
       </div>
     );
   }
