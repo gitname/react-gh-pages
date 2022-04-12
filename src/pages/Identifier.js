6ad9f2a96ca63be4ccb3aea8041afa12ab6472f4
Nathaniel Huish <nathanielhuish@gmail.com>
2022-04-11 21:20:34 -0500

    identifier

--- src/pages/Identifier.js
+++ src/pages/Identifier.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import './Identifier.css';
 
-const Identifier = () => {
-    return (
-        <div>
-            <h3>Sheeps</h3>
-            <div>
-                <img src="./sheeps.png" />
-                <img src="./sheeps.jpg" />
+class Identifier extends Component {
+    constructor() {
+        super();
+        this.state = {
+            imgSrc: 'https://laura.rochaprado.com/color-blind-simulator/img/conical-gradient.png',
+            textBoxValue: 'Enter image URL'
+        };
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(value) {
+        this.setState({
+            imgSrc: value
+        });
+    }
+
+    updateTextBox(value) {
+        this.setState({
+            textBoxValue: value
+        });
+    }
+
+    render() {
+        return (
+            <div class="identifier">
+                <h1>Color Identifier</h1>
+                <div class="input-area">
+                    <input type="text" id="text-box" value={this.state.textBoxValue} onChange={(event) => this.updateTextBox(event.target.value)} />
+                    <input type="submit" id="submit-button" value="Submit URL" onClick={(event) => this.handleChange(this.state.textBoxValue)} />
+                </div>
+                <div>
+                    <img src={this.state.imgSrc} width={500} height={500} objectFit={'scale-down'} />
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 export default Identifier;
