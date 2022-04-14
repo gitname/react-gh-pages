2c34a9fe1de848ad0b29712fe797ea0198a0511b
phiter07 <ppn4@txstate.edu>
2022-04-14 13:05:59 -0500

    added to resource. will fix layout later

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -3,9 +3,10 @@ import React from 'react';
 const Resources = () => {
     return (
         <div>
-            <h3>Cats</h3>
+            <h3>How Color Blindness Works</h3>
             <div>
-                <img src={require("./img/conical-gradient.png")} />
+                <body>An educational video by Human Interest about how colour blind people see the world and the science behind it </body>
+                <html src="https://www.youtube.com/watch?v=iNRQB5309yo" />
                 <img src="https://images.pexels.com/photos/45170/kittens-cat-cat-puppy-rush-45170.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
             </div>
         </div>
