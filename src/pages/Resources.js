6681a36d308298aad3ebd3500018ed0f8c5c762a
phiter07 <ppn4@txstate.edu>
2022-04-15 23:57:24 -0500

    Update Resources.js

--- src/pages/Resources.js
+++ src/pages/Resources.js
@@ -1,15 +1,29 @@
+
 import React from 'react';
 
 const Resources = () => {
     return (
         <div>
-            <h3>How Color Blindness Works</h3>
+            <h3 class ="mb-1">How Color Blindness Works</h3>
             <div>
-                <body>An educational video by Human Interest about how colour blind people see the world and the science behind it </body>
-                <html src="https://www.youtube.com/watch?v=iNRQB5309yo" />
-                <img src="https://images.pexels.com/photos/45170/kittens-cat-cat-puppy-rush-45170.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+            <p>An educational video&nbsp;by&nbsp;
+                <a href="https://www.youtube.com/channel/UCaPRCWnFAzeI3_tr--Qw5qg/videos">Human Interest&nbsp;</a>about how colour blind people see the world and the science behind it.</p>
+            </div>
+            <h4 class ="mb-1">All About Color Blindness: A Guide to Color Vision Deficiency for Kids (And Grown-ups Too)</h4>
+            <div>
+            <p>This book for kids (and grown-ups too) follows Corey on a journey that begins with problems caused by his color confusion.&nbsp; <br></br>“This one-of-a-kind story and fact book begins with Corey’s Story. Corey, a fourth-grader, explains how his color deficiency caused problems in kindergarten. A trip to the eye doctor reveals the problem. Corey’s color confusion is a physical condition that many people share and it has nothing to do with how smart he is.
+            Once he understands his color vision, Corey sails past color-dependent obstacles with a positive attitude and confident communication. Color Vision Deficiency isn’t the end of the world, says Corey. It’s just a different view of it!”&nbsp;
+                <br></br><a href="https://www.amazon.com/gp/product/0988561514?tag=wearecolor-20&geniuslink=true"> Buy on Amazon&nbsp;</a></p>
+            </div>
+            <h5 class ="mb-1">Stuff You Should Know Podcast: What is Color Blindness?</h5>
+            <div>
+            <p> This episode of the Stuff You Should Know Podcast talks about deuteranomaly, protanomaly, tritanomaly and all the various types of color deficiencies of the rods and cones that can cause colorblindness.
+            <br></br>Listen to the Podcast&nbsp;on&nbsp;
+                <a href="https://www.https://www.iheart.com/podcast/105-stuff-you-should-know-26940277/?pname=www.stuffyoushouldknow.com&sc=dnsredirect.com/channel/UCaPRCWnFAzeI3_tr--Qw5qg/videos">stuffyoushouldknow.com&nbsp;</a> </p>
             </div>
         </div>
+    
     );
 }
 export default Resources;
+
