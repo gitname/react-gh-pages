c06e63f30edb26081af614a25d11083185ca4f43
Balaji Shinde <66716239+BalajiShinde852@users.noreply.github.com>
2022-06-22 21:07:10 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          <h1>hello world!</h1>
         </p>
         <a
           className="App-link"
